318015cc04202729a978a30099f55a80ef48c587
joshua.lea <joshua.lea@TRNGLA13081.fdmgroup.local>
2016-08-29 16:05:27 +0100

    init

---
+++ FDMFaceino/src/main/java/com/FDMGroup/FdmFaceinoApplication.java
@@ -0,0 +1,12 @@
+package com.FDMGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdmFaceinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdmFaceinoApplication.class, args);
+	}
+}
