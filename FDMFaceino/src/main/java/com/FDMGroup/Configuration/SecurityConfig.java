1913564e1cdf9e21363d08d344749859df7a76c4
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-08-30 12:15:22 +0100

    update homepage html

--- FDMFaceino/src/main/java/com/FDMGroup/Configuration/SecurityConfig.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Configuration/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/**").authenticated()
+			.antMatchers("/admin/**").hasRole("ADMIN")
 		.and()
 		.formLogin()
 			.loginPage("/login")
