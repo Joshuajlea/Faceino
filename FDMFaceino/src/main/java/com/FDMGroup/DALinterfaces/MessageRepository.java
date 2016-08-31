86b9faa5d37ae58eb05c226dccfa81ec049b6050
tim.bell <tim.bell@TRNGLA13095.fdmgroup.local>
2016-08-31 16:13:41 +0100

    deleted MessageRepoInterface and UserRepoInterface

--- FDMFaceino/src/main/java/com/FDMGroup/DALinterfaces/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.FDMGroup.DALinterfaces;
-
-import com.FDMGroup.Entities.Message;
-
-public interface MessageRepository{
-	
-	public Message findByMessageId(String messageId);
-	
-}
--- FDMFaceino/src/main/java/com/FDMGroup/DALinterfaces/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.FDMGroup.DALinterfaces;
-
-import com.FDMGroup.Entities.User;
-
-public interface UserRepository {
-
-	public User findByLoginName(String loginName);
-
-}
