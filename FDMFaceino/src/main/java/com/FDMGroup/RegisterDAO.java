8bfd590fe009ebceff627592fc62f740d32ef88d
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-08-31 11:26:21 +0100

    RegisterDAO

--- FDMFaceino/src/main/java/com/FDMGroup/RegisterDAO.java
+++ FDMFaceino/src/main/java/com/FDMGroup/RegisterDAO.java
@@ -1,9 +1,17 @@
 package com.FDMGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.FDMGroup.Entities.User;
+
 public class RegisterDAO {
 	
-	public static boolean checkUsername(String username){
-		
+	public static boolean checkIfUsernameNOTUsed(String username){
+		List<User> allUsers = new ArrayList<User>(); 
+		if(allUsers.parallelStream().noneMatch(a -> a.getLoginName().equals(username))){
+			return true;
+		}
 		return false;
 	}
 	
