5c71e0bb8536e3dad69ecce6812e710b4c1afc6e
joshua.lea <joshua.lea@TRNGLA13053.fdmgroup.local>
2016-09-01 11:38:55 +0100

    rEGISTRATION CONTROLLER

--- FDMFaceino/src/main/java/com/FDMGroup/Controller/RegistrationController.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Controller/RegistrationController.java
@@ -1,14 +1,31 @@
 package com.FDMGroup.Controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.security.MessageDigest;
+import java.util.Calendar;
+import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.FDMGroup.RegisterDAO;
+import com.FDMGroup.Entities.Message;
+import com.FDMGroup.Entities.User;
 
 @Controller
 @SessionAttributes
@@ -58,5 +75,8 @@ public class RegistrationController {
 		}
 		return value;
 	}
-	
 }
+	
+
+	
+	
