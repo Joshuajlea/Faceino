2bb3dcd4382068487b13922d31291342f326fef9
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-09-05 11:08:20 +0100

    Service changes

--- FDMFaceino/src/main/java/com/FDMGroup/Services/RegistrationDataService.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Services/RegistrationDataService.java
@@ -7,4 +7,7 @@ import com.FDMGroup.Entities.User;
 public interface RegistrationDataService {
 	List<User> getAllUser();	
 	boolean registerUserToDatabase(User user);
+	List<User> getNewUsers();
+	List<User> getActivatedUsers();
+	List<User> getBlockedUsers();
 }
