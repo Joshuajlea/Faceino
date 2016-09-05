c299072172f5abe71ccfcdb160203b086dd17abc
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-09-05 11:59:12 +0100

    javascript excluding

---
+++ FDMFaceino/src/main/resources/static/js/navigationscript.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+function load_home(){
+	document.getElementById("contentPage").innerHTML= document.getElementById("hiddenContent").innerHTML;
+}
+   
+function load_profile(){
+	document.getElementById("contentPage").innerHTML= document.getElementById("hiddenProfile").innerHTML;
+}
+   
+function load_poker(){
+	document.getElementById("contentPage").innerHTML= document.getElementById("hiddenPoker").innerHTML;
+}
