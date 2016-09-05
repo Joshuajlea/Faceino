2ae7d2accfd54fd4d296a09d94c4f27be96af764
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-09-05 17:13:40 +0100

    admin stuff

--- FDMFaceino/src/main/java/com/FDMGroup/Controller/AdminController.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.FDMGroup.Controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
 
 import com.FDMGroup.Services.LoginDataService;
 import com.FDMGroup.Services.RegistrationDataService;
@@ -20,7 +23,7 @@ public class AdminController {
 	RegistrationDataService registerDataService = new RegistrationDataServiceImpl();
 	
 	@GetMapping("/admin")
-	public String enableUsers(HttpSession session, Model model, Authentication auth){
+	public String adminMainPage(HttpSession session, Model model, Authentication auth){
 		
 		session.setAttribute("userData", loginDataService.getUserDataFromDatabaseByName(auth.getName()));
 		
@@ -29,6 +32,24 @@ public class AdminController {
 		model.addAttribute("blockedUsers", registerDataService.getBlockedUsers());
 		
 		return "admin/adminPage";
+	}
+	
+	@PostMapping("/adminNewUser")
+	public String enableOrBlockUser(HttpServletRequest request){
+		String[] userListToEnable = request.getParameterValues("enable");
+		String[] userListToBeBlocked = request.getParameterValues("block");
+		
+		System.out.println("To Be enabled");
+		for(String s : userListToEnable){
+			System.out.println(s);
+		}	
+		
+		System.out.println("To Be blocked");
+		for(String s : userListToBeBlocked){
+			System.out.println(s);
+		}	
+		
+		return "redirect:/admin";
 	}
 	
 }
