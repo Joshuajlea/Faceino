83fa8ebff166fd913313fc05d9bbb333751b7b92
martin.mrowiec <martin.mrowiec@TRNGLA13097.fdmgroup.local>
2016-09-06 15:33:25 +0100

    session controlle

--- FDMFaceino/src/main/java/com/FDMGroup/Controller/SessionController.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Controller/SessionController.java
@@ -1,8 +1,5 @@
 package com.FDMGroup.Controller;
 
-import java.util.Collections;
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,30 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.FDMGroup.DALinterfaces.UserDAL;
-import com.FDMGroup.Entities.Message;
 import com.FDMGroup.Services.LoginDataService;
-import com.FDMGroup.Services.Implementation.LoginDataServiceImpl;
+import com.FDMGroup.Services.Implementation.ConversationDataServiceImpl;
 
 @Controller
 @SessionAttributes
 public class SessionController {
 	
-	LoginDataService loginDataService = new LoginDataServiceImpl();
 	@Autowired
-	UserDAL userDImpl;
+	LoginDataService loginDataService;
 	
+	@Autowired
+	ConversationDataServiceImpl conversationDataService;
 	
 	@RequestMapping("/home")
 	public String greetUser(HttpSession session, Model model, Authentication auth){
 		
 		session.setMaxInactiveInterval(600);
 		session.setAttribute("userData", loginDataService.getUserDataFromDatabaseByName(auth.getName()));
-		
-		List<Message> allMessages = userDImpl.getByLoginName(auth.getName()).getContent();
-		Collections.reverse(allMessages);
-		model.addAttribute("messages", allMessages);
-		
+				
+		session.setAttribute("conversations", conversationDataService.getConversationsByUserName(auth.getName()));
+		//session.setAttribute("conversations", Arrays.asList(new Conversation(),new Conversation(),new Conversation(),new Conversation()));
 		
 		return "homepage";
 	}
