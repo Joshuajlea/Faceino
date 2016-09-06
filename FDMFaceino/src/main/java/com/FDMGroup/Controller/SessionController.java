4312bba8d2375f98a150f71978e8b420c9f50465
tim.bell <tim.bell@TRNGLA13095.fdmgroup.local>
2016-09-06 12:03:14 +0100

    merge fix

--- FDMFaceino/src/main/java/com/FDMGroup/Controller/SessionController.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Controller/SessionController.java
@@ -2,7 +2,6 @@ package com.FDMGroup.Controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,26 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.FDMGroup.Services.LoginDataService;
-import com.FDMGroup.Services.Implementation.ConversationDataServiceImpl;
+import com.FDMGroup.Services.Implementation.LoginDataServiceImpl;
 
 @Controller
 @SessionAttributes
 public class SessionController {
 	
-	@Autowired
-	LoginDataService loginDataService;
-	
-	@Autowired
-	ConversationDataServiceImpl conversationDataService;
+	LoginDataService loginDataService = new LoginDataServiceImpl();
 	
 	@RequestMapping("/home")
 	public String greetUser(HttpSession session, Model model, Authentication auth){
 		
 		session.setMaxInactiveInterval(600);
 		session.setAttribute("userData", loginDataService.getUserDataFromDatabaseByName(auth.getName()));
-				
-		session.setAttribute("conversations", conversationDataService.getConversationsByUserName(auth.getName()));
-		//session.setAttribute("conversations", Arrays.asList(new Conversation(),new Conversation(),new Conversation(),new Conversation()));
 		
 		return "homepage";
 	}
--- FDMFaceino/src/main/java/com/FDMGroup/Services/Implementation/LoginDataServiceImpl.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Services/Implementation/LoginDataServiceImpl.java
@@ -1,13 +1,11 @@
 package com.FDMGroup.Services.Implementation;
 
-import org.springframework.stereotype.Component;
+import com.FDMGroup.Services.LoginDataService;
 
 import com.FDMGroup.DALimplementation.UserDALImp;
 import com.FDMGroup.DALinterfaces.UserDAL;
 import com.FDMGroup.Entities.User;
-import com.FDMGroup.Services.LoginDataService;
 
-@Component
 public class LoginDataServiceImpl implements LoginDataService {
 
 	UserDAL userDataAcessLayer = new UserDALImp();
