853adf0fc1f31c8f88a8c88c7933f91e95191369
arjun.kharel <arjun.kharel@TRNGLA13073.fdmgroup.local>
2016-09-06 16:45:05 +0100

    content controller and script.

---
+++ FDMFaceino/src/main/java/com/FDMGroup/Controller/ContentController.java
@@ -0,0 +1,82 @@
+package com.FDMGroup.Controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.FDMGroup.DALinterfaces.UserDAL;
+import com.FDMGroup.Entities.Message;
+import com.FDMGroup.Entities.User;
+import com.FDMGroup.Repositories.InMemoryUserRepository;
+import com.FDMGroup.Services.LoginDataService;
+import com.FDMGroup.Services.Implementation.LoginDataServiceImpl;
+
+@Controller
+
+public class ContentController {
+
+	LoginDataService loginDataService = new LoginDataServiceImpl();
+	private List<Message> posts = new ArrayList<Message>();
+
+	/*
+	 * @PostMapping("/postcontent") public String postContent(HttpServletRequest
+	 * request, Authentication auth) {
+	 * 
+	 * System.out.println("Request param: " + request.getParameter("content"));
+	 * 
+	 * String tempUser = auth.getName().toString();
+	 * loginDataService.getUserDataFromDatabaseByName(tempUser) .addContent(new
+	 * Message(tempUser, request.getParameter("content")));
+	 * 
+	 * return "redirect:/home"; }
+	 */
+
+	@Autowired
+	UserDAL userDImpl;
+
+	@GetMapping("/posts/{text}")
+	public String postStuff(Model model, HttpSession session, HttpServletRequest request,
+			@PathVariable("text") String text, Authentication auth) {
+		model.addAttribute("test", "Working");
+
+		userDImpl.getByLoginName(auth.getName()).getContent().add(new Message(auth.getName(), text));
+
+		sessionThings(session);
+		return "temp_content :: logo";
+	}
+
+	@GetMapping("/posts")
+	public String postStuff(Model model, HttpSession session) {
+		model.addAttribute("test", "Working");
+
+		sessionThings(session);
+
+		return "temp_content :: logo";
+	}
+
+	private void sessionThings(HttpSession session) {
+		posts.clear();// clear the list to avoid duplicate entries...
+		for (User user : InMemoryUserRepository.getInstance().getAll()) {
+			// posts.addAll(loginDataService.getUserDataFromDatabaseByName(auth.getName()).getContent());
+			posts.addAll(user.getContent());
+		}
+		Collections.sort(posts);
+		Collections.reverse(posts);
+
+		session.setAttribute("posts", posts);
+	}
+
+}
