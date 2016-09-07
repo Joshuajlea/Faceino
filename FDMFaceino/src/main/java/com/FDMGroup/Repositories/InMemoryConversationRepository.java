1e4319795b79a58ee011a9ace7608c5d3942a335
tim.bell <tim.bell@TRNGLA13095.fdmgroup.local>
2016-09-07 09:51:29 +0100

    added default conversation and messages to ConRepo; fixed message loading for different groups

--- FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
@@ -25,7 +25,17 @@ public class InMemoryConversationRepository {
 		List<User> receiver = InMemoryUserRepository.getInstance().getAll();
 		Conversation con = new Conversation(receiver);
 		con.addMessage(new Message("sebastian.verfers@fdmgroup.com", "das ist ein test"));
+		
+		List<User> receiver2 = new ArrayList<User>();
+		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("tim.bell@fdmgroup.com"));
+		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("sebastian.verfers@fdmgroup.com"));
+		Conversation con2 = new Conversation(receiver2);
+		con2.addMessage(new Message("sebastian.verfers@fdmgroup.com", "das ist ein test"));
+		con2.addMessage(new Message("tim.bell@fdmgroup.com", "Tim here, how are you"));
+		con2.addMessage(new Message("sebastian.verfers@fdmgroup.com", "Good, thanks"));
+		con2.addMessage(new Message("tim.bell@fdmgroup.com", "cool, dude, this works"));
 		conversations.add(con);
+		conversations.add(con2);
 	}
 	
 	public boolean addConversation(Conversation con){
