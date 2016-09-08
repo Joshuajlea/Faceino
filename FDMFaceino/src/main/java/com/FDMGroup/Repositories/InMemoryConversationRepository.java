9220cbf65c03efe108875cbc83abf630372b0a12
tim.bell <tim.bell@TRNGLA13095.fdmgroup.local>
2016-09-08 11:42:06 +0100

    avoid merge conflict

--- FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.FDMGroup.Entities.Conversation;
+import com.FDMGroup.Entities.Message;
+import com.FDMGroup.Entities.User;
 
 
 public class InMemoryConversationRepository {
@@ -19,6 +21,11 @@ public class InMemoryConversationRepository {
 	}	
 	
 	private InMemoryConversationRepository(){	
+		List<User> receiver2 = new ArrayList<User>();
+ 		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("tim.bell@fdmgroup.com"));
+ 		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("sebastian.verfers@fdmgroup.com"));
+ 		Conversation con2 = new Conversation(receiver2);
+ 		con2.addMessage(new Message("tim.bell@fdmgroup.com", "Tim here, how are you"));
 	}
 	
 	public boolean addConversation(Conversation con){
