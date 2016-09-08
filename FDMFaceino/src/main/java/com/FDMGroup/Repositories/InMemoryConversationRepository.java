eef612ba346663fd044833566cae4a92de56cdb4
tim.bell <tim.bell@TRNGLA13095.fdmgroup.local>
2016-09-08 11:45:45 +0100

    deleted default group chats

--- FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
+++ FDMFaceino/src/main/java/com/FDMGroup/Repositories/InMemoryConversationRepository.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.FDMGroup.Entities.Conversation;
-import com.FDMGroup.Entities.Message;
-import com.FDMGroup.Entities.User;
 
 
 public class InMemoryConversationRepository {
@@ -20,18 +18,7 @@ public class InMemoryConversationRepository {
 		return INSTANCE;
 	}	
 	
-	private InMemoryConversationRepository(){	
-		// default conversation only for testing
-		List<User> receiver = InMemoryUserRepository.getInstance().getAll();
-		
-		List<User> receiver2 = new ArrayList<User>();
-		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("tim.bell@fdmgroup.com"));
-		receiver2.add(InMemoryUserRepository.getInstance().getByLoginName("sebastian.verfers@fdmgroup.com"));
-		Conversation con2 = new Conversation(receiver2);
-		con2.addMessage(new Message("tim.bell@fdmgroup.com", "Tim here, how are you"));
-		con2.addMessage(new Message("sebastian.verfers@fdmgroup.com", "Good, thanks"));
-		con2.addMessage(new Message("tim.bell@fdmgroup.com", "cool, dude, this works"));
-		conversations.add(con2);
+	private InMemoryConversationRepository(){
 	}
 	
 	public boolean addConversation(Conversation con){
